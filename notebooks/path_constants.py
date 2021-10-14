8525f27402be06953f9262d4508c896849d8f892
Olga Botvinnik <olga.botvinnik@gmail.com>
2021-10-14 16:52:36 -0700

    Change all data_sm paths

--- notebooks/path_constants.py
+++ notebooks/path_constants.py
@@ -6,6 +6,18 @@ DATA_FOLDER = os.path.join("..", "kmer-homology-data")
 # Raw data = straight from the sequencer, or downloaded from a database
 RAWDATA_FOLDER = os.path.join(DATA_FOLDER, "00--rawdata")
 
+# Quest for Orthologs 2019 folder
+QFO_FOLDER = os.path.join(RAWDATA_FOLDER, "quest-for-orthologs", "2019")
+
+# Quest for Orthologs 2019 Eukaryota data
+QFO_EUKARYOTA_FOLDER = os.path.join(
+    RAWDATA_FOLDER, "quest-for-orthologs", "2019", "Eukaryota"
+)
+
+# BUSCO Mammalia folder
+BUSCO_MAMMALIA_FOLDER = os.path.join(RAWDATA_FOLDER, "busco", "mammalia_odb10", "info")
+
+
 # Processed data = data that has been maniuplated in some way from the raw data
 PROCESSED_DATA_FOLDER = os.path.join(DATA_FOLDER, "01--processed-data")
 
