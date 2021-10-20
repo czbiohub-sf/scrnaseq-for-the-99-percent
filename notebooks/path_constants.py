00452b17bb5e11f0aa2a2b040e81c4eaa683c2af
Olga Botvinnik <olga.botvinnik@gmail.com>
2021-10-20 13:24:37 -0700

    Udpate sklearn notebooks

--- notebooks/path_constants.py
+++ notebooks/path_constants.py
@@ -14,12 +14,15 @@ QFO_EUKARYOTA_FOLDER = os.path.join(
     RAWDATA_FOLDER, "quest-for-orthologs", "2019", "Eukaryota"
 )
 
+# Ortholog database (OrthoDB) folder
+ORTHODB_FOLDER = os.path.join(RAWDATA_FOLDER, "orthodb", "v10.1")
+
 # BUSCO Mammalia folder
 BUSCO_MAMMALIA_FOLDER = os.path.join(RAWDATA_FOLDER, "busco", "mammalia_odb10", "info")
 
-
 # Processed data = data that has been maniuplated in some way from the raw data
 PROCESSED_DATA_FOLDER = os.path.join(DATA_FOLDER, "01--processed-data")
+
 
 # Data related to orpheum benchmarking
 ORPHEUM_BENCHMARKING_FOLDER = os.path.join(
@@ -27,4 +30,12 @@ ORPHEUM_BENCHMARKING_FOLDER = os.path.join(
 )
 MAMMALIA_BUSCO_SUBSET_FOLDER = os.path.join(
     ORPHEUM_BENCHMARKING_FOLDER, "mammalia_busco_subsets"
+)
+
+# Human simulated reads -- output from Polyster simulated reads
+SIMULATED_READS_FOLDER = os.path.join(ORPHEUM_BENCHMARKING_FOLDER, "simulated", "human")
+
+# Folder of output of Orpheum translate on human reads
+ORPHEUM_PIPELINE_RESULTS_FOLDER = os.path.join(
+    ORPHEUM_BENCHMARKING_FOLDER, "orpheum-translate-on-simulated-human-data-results"
 )
